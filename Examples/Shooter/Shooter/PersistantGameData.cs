db9601a46b66c294ce377b01f0ca7ae6dae32f29
balaam <balaam@6ea7a62d-faad-b55e-fb2e-51d7e55f402d>
2010-07-10 20:46:18 +0000

    Add Shooter example
    Notes
    -Font loading changed due to change in the Engine code (from the code accompanying the book)

---
+++ Examples/Shooter/Shooter/PersistantGameData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shooter
+{
+    class PersistantGameData
+    {
+        public bool JustWon { get; set; }
+        public LevelDescription CurrentLevel { get; set; }
+        public PersistantGameData()
+        {
+            JustWon = false;
+        }
+    }
+
+}
